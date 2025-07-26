f13707490df4e38fae128946ca53fc243fdcfea4
Helmi Hernandez <75708935+helmidh1@users.noreply.github.com>
2025-07-26 10:35:09 -0400

    saving

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "973e439a8b4cf667f6cbddaf2371e91a
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "f7b7c4f7dc7f5d926432267b9be6c07d",
+"flutter_bootstrap.js": "0a8631ebafc337cd3dcd32c2bb59416c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
